e9f0a751cadfcf51bd482bbe659af79b6db34a1f
Tommaso Sgroi <sgroi.1852992@studenti.uniroma1.it>
2023-12-12 10:56:21 +0100

    new Dataset parser and user data handlers. TODO Couple generator

--- parse_dataset.py
+++ parse_dataset.py
@@ -35,16 +35,42 @@ keystrokes = '''100390_keystrokes.txt
 d = {}
 
 
+class DataParser(object):
+    def __init__(self, files, *, base_path='', types=None):
+        self.types = [int, int, str, str, int, int, int, str, int] if types is None else types
+        self.files = files
+        self.base_path = base_path
+        self.user_data = []
+
+    def parse(self):
+        for file in self.files:
+            with open(self.base_path + file, 'r', encoding='utf-8') as fl:
+                headers = fl.readline().strip().split('\t')
+                lines = fl.readlines()
+                self.user_data.append(UserData(lines, headers=headers, headers_types=self.types))
+
+
 # Parsing dei dati
 class UserData(object):
-    def __init__(self, lines) -> None:
+    def __init__(self, lines, *, headers=None, headers_types=None, add_difference_press_release=True) -> None:
         # list of phrases
+        self.headers = [] if headers is None else headers
+        self.headers_types = [] if headers_types is None else headers_types
         self.phrases = []
         self.train_averages = []
         self.test_averages = []
 
         self.id = int(lines[0].split('\t')[0])
         self._split_phrases(lines)
+        if add_difference_press_release:
+            self._add_difference_press_release()
+
+    def _add_difference_press_release(self):
+        press_time_index = self.headers.index('PRESS_TIME')
+        release_time_index = self.headers.index('RELEASE_TIME')
+        for phrase in self.phrases:
+            for line in phrase:
+                line.append(line[release_time_index] - line[press_time_index])
 
     # calculate error rate = how many wrong letters (either in substitution or in addition/subtraction) have been typed
     def calc_error_rate(self, user_in, user_out):
@@ -57,27 +83,24 @@ class UserData(object):
         return err_rate
 
     # method that split lines to get datas about sentences
-    def _split_phrases(self, lines):
-        def select_data(data):
-            press_time = data[5]
-            release_time = data[6]
-            return int(press_time), int(release_time)
+    def _convert_type(self, line):
+        for index in range(len(self.headers_types)):
+            if type(line[index]) is not self.headers_types[index]:
+                line[index] = self.headers_types[index](line[index])
+        return line
 
+    def _split_phrases(self, lines):
         sentence = -1
         data_line = []
-        for l in lines:
-            l_split = l.split('\t')
+        for line in lines:
+            l_split = line.split('\t')
             if int(l_split[1]) != sentence and sentence != -1:
-                err_rate = self.calc_error_rate(l_split[2], l_split[3])
-                # efficiently put the error rate as first value
-                # data_line.append(data_line[0])
-                # data_line[0] = err_rate
-                data_line.append(err_rate)
                 self.phrases.append(data_line)
                 data_line = []
 
             sentence = int(l_split[1])
-            l_split = select_data(l_split)
+            l_split[-1] = l_split[-1].rstrip()  # remove \n
+            l_split = self._convert_type(l_split)
             data_line.append(l_split)
 
     # calculate average for press_time, release_time
@@ -104,32 +127,34 @@ class UserData(object):
 
 
 # all lines are parsed and the dict is built
-for k in keystrokes:
-    with open(PATH + k, 'r', encoding='utf-8') as f:
-        from time import time
 
-        start_time = time()
-        u = UserData(f.readlines()[1:])
-        print(u.phrases)
-        avgs = u.calc_avg()  # avg_press, avg_release, avg_err
-        d[u.id] = avgs
-        print('time spent', time() - start_time)
+from time import time
+
+start_time = time()
+data_parser = DataParser(keystrokes, base_path=PATH)
+data_parser.parse()
+print(data_parser.user_data)
+print('time spent', time() - start_time)
 print(d)
 
-# all lines are parsed and the dict is built
-for k in keystrokes:
-    with open(PATH + k, 'r', encoding='utf-8') as f:
-        from time import time
 
-        start_time = time()
-        u = UserData(f.readlines()[1:])
-        print(u.phrases)
-        avgs = u.calc_avg()  # avg_press, avg_release, avg_err
-        d[u.id] = avgs
-        print('time spent', time() - start_time)
-print(d)
+class CoupleGenerator(object):
+    def __init__(self, users_data):
+        self.users_data = users_data
 
-# data to use in test and train
+    def split_dataset(self):
+        pass
+
+    def generate_positive_couples(self):
+        import itertools as it
+
+        pass
+
+    def generate_negative_couples(self):
+        pass
+
+
+'''# data to use in test and train
 test_data = {k: v[floor(len(v) / 2):] for k, v in zip(d.keys(), d.values())}
 
 positive_couples = []
@@ -152,12 +177,11 @@ for k1 in range(len(keys)):
 print(positive_couples, len(positive_couples))
 print(negative_couples, len(negative_couples))
 
-
 train_X1, test_X1 = [], []
 train_X2, test_X2 = [], []
 train_y, test_y = [], []
 for i in range(len(positive_couples)):
-    if i < len(positive_couples)//2:
+    if i < len(positive_couples) // 2:
         train_X1.append(positive_couples[i][0])
         train_X2.append(positive_couples[i][1])
         train_y.append(1)
@@ -167,7 +191,7 @@ for i in range(len(positive_couples)):
         test_y.append(1)
 
 for i in range(len(negative_couples)):
-    if i < len(negative_couples)//2:
+    if i < len(negative_couples) // 2:
         train_X1.append(negative_couples[i][0])
         train_X2.append(negative_couples[i][1])
         train_y.append(0)
@@ -186,3 +210,4 @@ test_X1 = np.array(test_X1).astype(np.float32)
 test_X2 = np.array(test_X2).astype(np.float32)
 
 pass
+'''
