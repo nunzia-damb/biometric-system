00b73ccddd99848684e64a04455292d33eb50ebc
Tommaso-Sgroi <sgroi_tom@yahoo.com>
2024-01-04 16:23:24 +0100

    Added evaluation file

---
+++ plot_checkpoint.py
@@ -0,0 +1,46 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.metrics import RocCurveDisplay, DetCurveDisplay, confusion_matrix, classification_report
+import seaborn as sns
+
+from test_siamese import y_test, a_test, b_test, shape as input_shape, create_siamese_model
+
+
+def load_model(epoch):
+    # Load the previously saved weights
+    mymodel = create_siamese_model(input_shape)
+    mymodel.load_weights(f'checkpoints/cp-{epoch:04d}.ckpt')
+    return mymodel
+
+
+def report(model, *, a_test, b_test, y_test):
+    prediction = (model.predict([a_test, b_test], verbose=0).ravel()).astype(np.float64)
+    RocCurveDisplay.from_predictions(y_test, prediction)
+    DetCurveDisplay.from_predictions(y_test, prediction)
+    plt.show()
+
+    prediction = (prediction > 0.5).astype(np.float64)
+    cm = confusion_matrix(y_test, prediction)
+    print(cm)
+    ax = sns.heatmap(cm, annot=True, cmap='Blues')
+
+    ax.set_title('Seaborn Confusion Matrix with labels\n\n')
+    ax.set_xlabel('\nPredicted Values')
+    ax.set_ylabel('Actual Values ')
+
+    ## Ticket labels - List must be in alphabetical order
+    ax.xaxis.set_ticklabels(['False', 'True'])
+    ax.yaxis.set_ticklabels(['False', 'True'])
+
+    ## Display the visualization of the Confusion Matrix.
+    plt.show()
+    cr = classification_report(y_test, prediction, labels=None,
+                               target_names=['0', '1'],
+                               digits=4)
+    print(cr)
+
+
+if __name__ == '__main__':
+    chkpt = int(input('insert checkpoint '))
+    siamese = create_siamese_model(input_shape)
+    report(siamese, b_test=b_test, a_test=a_test, y_test=y_test)
--- test_siamese.py
+++ test_siamese.py
@@ -1,16 +1,11 @@
 import numpy as np
-import sklearn.preprocessing
 from keras.models import *
 from keras.layers import *
 from keras.optimizers import Adam
-from keras.losses import binary_crossentropy
 from keras import backend as K
 from keras.layers import Lambda
-from keras.src.callbacks import EarlyStopping
 from matplotlib import pyplot as plt
-from sklearn.metrics import roc_curve, auc, confusion_matrix, RocCurveDisplay, DetCurveDisplay
 from sklearn.preprocessing import StandardScaler
-import seaborn as sns
 
 
 # Function to generate random sequences
@@ -94,20 +89,20 @@ def create_siamese_model(input_shape):
     feature_extractor = create_feature_extractor(input_shape)
 
     # Connect both inputs to the shared feature extractor
-    feature_vector_A = feature_extractor(input_a)
-    feature_vector_B = feature_extractor(input_b)
+    feature_vector_a = feature_extractor(input_a)
+    feature_vector_b = feature_extractor(input_b)
 
     # concat = Concatenate()([feature_vector_A, feature_vector_B])
     # dense = Dense(64, activation='relu')(concat)
 
     # distance = Lambda(euclidean_distance, output_shape=(8))([feature_vector_A, feature_vector_B])
     # concat = Concatenate()([feature_vector_A, feature_vector_B])
-    l1_distance = Lambda(lambda tensors: K.abs(tensors[0] - tensors[1]))([feature_vector_A, feature_vector_B])
+    l1_distance = Lambda(lambda tensors: K.abs(tensors[0] - tensors[1]))([feature_vector_a, feature_vector_b])
 
     decision_module = create_decision_module(l1_distance)
     decision_module = Flatten()(decision_module)
     decision_module = Dense(1, activation='sigmoid')(decision_module)
-    siamese_model = Model(inputs=[input_a, input_b], outputs=decision_module)
+    sm = Model(inputs=[input_a, input_b], outputs=decision_module)
 
     # dense = Dense(64, activation='relu')(l1_norm)
     # flattened = Flatten()(dense)
@@ -117,74 +112,76 @@ def create_siamese_model(input_shape):
 
     # model = Model(inputs=[input_a, input_b], outputs=output)
 
-    return siamese_model
+    return sm
 
 
-# Reshape the data
+def load_callbacks():
+    import os
+    from keras.src.callbacks import EarlyStopping, History, ModelCheckpoint
+
+    history = History()
+    checkpoint_path = "checkpoints" + os.sep + "cp-{epoch:04d}.ckpt"
+    # checkpoint_dir = os.path.dirname(checkpoint_path)
+
+    # Create a callback that saves the model's weights
+    cp_callback = ModelCheckpoint(filepath=checkpoint_path,
+                                  save_weights_only=True,
+                                  save_best_only=False,
+                                  monitor='val_accuracy',
+                                  mode='max',
+                                  verbose=1)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
+    return [history, cp_callback, early_stopping]
+
+def normalize_dataset(X_train, X_test):
+    scaler = StandardScaler()
+
+    x_trainnn = np.array(X_train)
+    nsamples, nx, ny, nz = x_trainnn.shape
+    x_trainnn = x_trainnn.reshape((nsamples, nx * ny * nz))
+    x_trainnn = scaler.fit_transform(x_trainnn)
+    x_trainnn = x_trainnn.reshape((nsamples, nx, ny, nz))
+
+    X_test = np.array(X_test)
+    nsamples, nx, ny, nz = X_test.shape
+    X_test = X_test.reshape((nsamples, nx * ny * nz))
+    X_test = scaler.fit_transform(X_test)
+    X_test = X_test.reshape((nsamples, nx, ny, nz))
+
+    a, b = x_trainnn[:, 0, :, :], x_trainnn[:, 1, :, :]
+
+    a_test, b_test = X_test[:, 0, :, :], X_test[:, 1, :, :]
+
+    return a, b, a_test, b_test
+
 
 X_test, X_train, y_test, y_train, shape = generate_pairs(100)
 
-# Create siamese model
-print(shape)
-input_shape = shape
-siamese_model = create_siamese_model(input_shape)
+a, b, a_test, b_test = normalize_dataset(X_train, X_test)
+del X_test, X_train
 
-siamese_model.summary()
+if __name__ == '__main__':
+    # Create siamese model
+    print(shape)
+    input_shape = shape
+    siamese_model = create_siamese_model(input_shape)
 
-# Compile the model
-siamese_model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.001, ), metrics=['accuracy'])
+    siamese_model.summary()
 
-scaler = StandardScaler()
-
-X_trainnn = np.array(X_train)
-nsamples, nx, ny, nz = X_trainnn.shape
-X_trainnn = X_trainnn.reshape((nsamples, nx * ny * nz))
-X_trainnn = scaler.fit_transform(X_trainnn)
-X_trainnn = X_trainnn.reshape((nsamples, nx, ny, nz))
-
-X_test = np.array(X_test)
-nsamples, nx, ny, nz = X_test.shape
-X_test = X_test.reshape((nsamples, nx * ny * nz))
-X_test = scaler.fit_transform(X_test)
-X_test = X_test.reshape((nsamples, nx, ny, nz))
-
-a, b = X_trainnn[:, 0, :, :], X_trainnn[:, 1, :, :]
-
-a_test, b_test = X_test[:, 0, :, :], X_test[:, 1, :, :]
-
-early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+    # Compile the model
+    siamese_model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.001, ), metrics=['accuracy'])
 
 
-# Train the model
-siamese_model.fit([a, b], y_train, epochs=10, batch_size=100, steps_per_epoch=200,
-                  validation_data=([a_test, b_test], y_test), callbacks=[early_stopping],
-                  validation_freq=1, use_multiprocessing=True, workers=7, verbose=1, shuffle=True)
-# Evaluate the model on the test set
+    # Train the model
+    siamese_model.fit([a, b], y_train, epochs=10, batch_size=100, steps_per_epoch=200,
+                      validation_data=([a_test, b_test], y_test), callbacks=load_callbacks(),
+                      validation_freq=1, use_multiprocessing=True, workers=7, verbose=1, shuffle=True)
+    # Evaluate the model on the test set
 
-evaluation = siamese_model.evaluate([a_test, b_test], y_test, verbose=0)
+    evaluation = siamese_model.evaluate([a_test, b_test], y_test, verbose=0)
 
-print("Test Loss:", evaluation[0])
-print("Test Accuracy:", evaluation[1])
-
-prediction = (siamese_model.predict([a_test, b_test], verbose=0).ravel()).astype(np.float64)
-RocCurveDisplay.from_predictions(y_test, prediction)
-DetCurveDisplay.from_predictions(y_test, prediction)
-plt.show()
-
-prediction = (prediction > 0.5).astype(np.float64)
-cm = confusion_matrix(y_test, prediction)
-print(cm)
-ax = sns.heatmap(cm, annot=True, cmap='Blues')
-
-ax.set_title('Seaborn Confusion Matrix with labels\n\n')
-ax.set_xlabel('\nPredicted Values')
-ax.set_ylabel('Actual Values ')
-
-## Ticket labels - List must be in alphabetical order
-ax.xaxis.set_ticklabels(['False', 'True'])
-ax.yaxis.set_ticklabels(['False', 'True'])
-
-## Display the visualization of the Confusion Matrix.
-plt.show()
+    print("Test Loss:", evaluation[0])
+    print("Test Accuracy:", evaluation[1])
 
 pass
